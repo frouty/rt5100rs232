6b6e2efaf47a1cbd50783ffa07d02f37364d5bbc
Laurent FRANCOIS <francois.oph@gmail.com>
2016-05-24 20:54:38 +1100

    update

--- parse_10.py
+++ parse_10.py
@@ -2,6 +2,7 @@
 """
 """
 import formatSCAvalues_final as trimSCA
+import re
 
 SCAdict={'f':('subjectdata','FarVisionSCA'),
               'n':('subjectdata','NearVisionSCA'),
@@ -18,6 +19,42 @@ keysOS = ('sph_os','cyl_os','axe_os')
 #constant
 coupures = [28, 30, 36, 42, 45] 
 
+def trimzero(val):
+    """Trim zero value if there is one at the 2nd decimal
+    needed if there is a selection fields with no zero at the 2nd decimal
+    in Odoo
+    
+    val : string from the rt5100
+    
+    example: '+ 2.20' return '+ 2.2'
+    example: '100' return '100'
+    """
+    res = val
+    regex = r'\.\d0'
+    if re.search(regex, val, flags = 0):
+        #match = re.search(regex, val, flags = 0)
+        #print 'match:{}'.format(match)
+        if val[-1] == '0':
+            res = val[:-1]
+    return res
+
+def trimspace_regex(val):
+    """Trim the space after sign +/- if there is one in val
+    val : string from rt-5100
+    eg val = '+ 2,00'  --> return '+2.00'
+    eg val ='  0,00'   --> return '  0.00'
+
+    return the trimed string
+    """
+    regex=r'^[+-] ' # don't forget the space at the end of the regex
+    if re.search(regex, val, flags = 0):
+        #match = re.search(regex, val, flags = 0)
+        #print 'match:{}'.format(match)
+        val=val[:1]+val[2:]
+    return val
+
+
+
 def get_values(filter):
     """Return a dictionnary of the values
     
@@ -25,11 +62,16 @@ def get_values(filter):
         Il y a un probleme quand il n'y a aucune ligne qui correspond au filtre
         log_path : path to the file with the datas given by RT-5100 device
         
+        values: eg : values:['OR', '- 2.75', '  0.00', '  0']
+        
+        morceaux : eg : ['2016-05-02T05:37:02.410503\t\x02', 'OR', '- 2.75', '  0.00', '  0', '\r\n']
         return : tuple .vals:({'cyl_od': '- 1.25', 'axe_od': '125', 'sph_od': '+ 5.50'}, {'sph_os': '+ 5.50', 'axe_os': '125', 'cyl_os': '- 1.25'})
     """
     coupures = [28, 30, 36, 42, 45] 
     filterR = filter+'R'
+    print 'filterR:{}'.format(filterR)
     filterL = filter+'L'
+    print 'filterL;{}'.format(filterL)
    
     if filter not in ['f','n','F','N','O']:
         print "print there is a problem with your filter:{}".format(filter)
@@ -40,34 +82,75 @@ def get_values(filter):
             for line in file.readlines():
                 if line.find(filterR) != -1 or line.find(filterL) != -1:
                     morceaux = [line[i:j] for i, j in zip([0] + coupures, coupures + [None])] # on coupe les lignes en morceaux qui isolent les champs.
-                    values=morceaux[1:5] # on élimine le champ date et on récupere que les champs datas.
+                    values=morceaux[1:5] # on élimine le champ date et on récupere que les champs datas.SCA dans une liste
                     # morceaux[2] --> sph 6 bits datas
                     # morceaux[3] --> cyl 6 bits datas
                     # morceaux[4] --> axes 6 bits datas
                     # morceaux[2:5] donne ['S','C','A']
                     # lets format the str to fit the odoo selection for SCA
-                    values[-1]=values[-1].strip()
-                    for idx in (1,2):
-                        print values[idx]
-                        
-                    print values
-                    import pdb;pdb.set_trace()        
-                    res = [d.strip() for d in morceaux[2:5] if len(d)==3]
+                    values=[val.strip() for val in values]
+                    print 'values stripped:{}'.format(values)
+                    values=[trimspace_regex(val) for val in values]
+                    print 'values trimspace:{}'.format(values)
+                    values=[trimzero(val) for val in values]
+                    print 'values trimzero:{}'.format(values)
+                    print 'formated values:{}'.format(values)
                     
                     if values[0] == filterR: # On filtre pour l'oeil droit
                         valuesOD=dict(zip(keysOR,values[1:]))
-                        #print 'valuesOD:{}'.format(valuesOD)
+                        print 'valuesOD:{}'.format(valuesOD)
                     else:
                         valuesOS=dict(zip(keysOS,values[1:])) #on filtre pour l'oeil gauche
-                        #print 'valuesOS:{}'.format(valuesOS)
+                        print 'valuesOS:{}'.format(valuesOS)
 
         except IOError, (error, strerror):
             print "I/O Error(%s): %s" % (error, strerror)
-   # print 'locals():{}'.format(locals())
-    #print 'done'
-   # print '-'*10
+
     res=[valuesOD, valuesOS]
+    print 'res:{}'.format(res)
     return res
+
+def trimspace(val):
+    idx = 1
+    val = val.strip()
+    if val[1] == ' ':
+        res = val[:idx] + val[(idx + 1):]
+    else:
+        res = val
+    return res
+
+def trimzero(val):
+    res = val
+    regex = r'\.\d0'
+    if re.search(regex, val, flags = 0):
+        match = re.search(regex, val, flags = 0)
+        print 'match:{}'.format(match)
+        if val[-1] == '0':
+            res = val[:-1]
+    return res
+
+def main_trim(val):
+    """ Apply trimzero and trimspace
+    
+        return: string the trimed val
+    """
+    print 'in main_trim val is {}'.format(val)
+    res = trimspace(trimzero(val))
+    print 'in main_trim res is {}'.format(res)
+    return res
+
+def main_get(vals):
+    """Get the SCA values ready for ODOO
+    
+        vals tuple of dictionnaries from get values
+        
+        return: tuple of dictionnaryes of values trimed
+    """
+    for i in vals:
+        for k, v in i.iteritems():
+            i[k] = main_trim(v)
+    return vals
+
 
 def trim(vals):
     """Trim the values of the dictionnary
@@ -86,28 +169,7 @@ def trim(vals):
 
 if __name__ == '__main__':
     vals = get_values("O")
-    print 'vals:{}'.format(vals)
-    print 'OD:{}'.format(vals[0])
-    print 'OG:{}'.format(vals[1])
-    print '='*10
-    print "now let's trim"
-    for i in vals: #j'itere sur chaque dictionnaire.
-        print 'i:{}'.format(i) # dict pour un oeil . vals[0] dictionnaire 1 et vals[1] dictionnaire 2
-        print 'values:{}'.format(i.values()) # list des values
-        for k, v in i.iteritems(): # est un value
-            print 'v :{}'.format(v)
-            i[k]=trimSCA.main_trim(v)
-            #[k]=trimSCA.trimspace(trimSCA.trimzero(v))
-#             i[k]=trimSCA.trimspace(v)
-#             i[k]=trimSCA.trimzero(i[k])
-            #i[k]=trim.trimzero(i[k])
-            #trimed = trim.trimspace(v)
-            #print 'trim space : {}'.format(i[k])
-            #trimed = trim.trimzero(trimed)
-            #print 'trim zero : {}'.format(trimed)
-        print '-'*10
-        print i.values()
-        print i
+    
     #===========================================================================
     # for i in SCAdict.keys():
     #     print '='*10
