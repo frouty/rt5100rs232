896f0b09d2e0323de0842a52de5234e3cd307931
Laurent FRANCOIS <francois.oph@gmail.com>
2016-09-18 16:21:25 +1100

    add fix" "

--- autostart/hello_world.py
+++ autostart/hello_world.py
@@ -6,6 +6,7 @@ logging.warning( 'Starts Hello World' )
 try:
     while True:
         print "Hello World"
-        sleep( 60 )
+	logging.warning('Je suis la')
+        sleep( 5 )
 except KeyboardInterrupt, e:
     logging.warning( "Stopping..." )
